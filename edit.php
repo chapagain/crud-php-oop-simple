db866cda93c937371aacf8c1567c6f779c9fbeec
Mukesh Chapagain <mukesh.chapagain@gmail.com>
2016-11-22 22:17:45 +0545

    Fix for full text not displayed in edit form text fields

--- edit.php
+++ edit.php
@@ -29,15 +29,15 @@ foreach ($result as $res) {
 		<table border="0">
 			<tr> 
 				<td>Name</td>
-				<td><input type="text" name="name" value=<?php echo $name;?>></td>
+				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
 			</tr>
 			<tr> 
 				<td>Age</td>
-				<td><input type="text" name="age" value=<?php echo $age;?>></td>
+				<td><input type="text" name="age" value="<?php echo $age;?>"></td>
 			</tr>
 			<tr> 
 				<td>Email</td>
-				<td><input type="text" name="email" value=<?php echo $email;?>></td>
+				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
 			</tr>
 			<tr>
 				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
